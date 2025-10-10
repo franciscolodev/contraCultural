a086028d11d66ede170945dd65002b785c65e2c6
Fran <franciscolodev@gmail.com>
2025-10-09 21:14:04 -0300

    vamosQueVa2

--- src/i18n/design/pt.js
+++ src/i18n/design/pt.js
@@ -1,16 +1,49 @@
+//export default {
+  //mate: {
+    //description: "Designers, fotógrafos e artistas criativos — mostre aqui o seu melhor trabalho.",
+//    title: "Designer",
+//  },
+//  btns: { backToTop: 'Voltar ao topo' },
+//  page_works: { title: '{{name}} - Trabalhos' },
+//  footer: { cr: 'Todos os direitos reservados.', icp: '(Río Cuarto, Córdoba, Argentina) ' },
+//  menus: { all: 'Tudo', about: 'Sobre', works: 'Trabalhos', contact: 'Contato' },
+  //page_404: {
+    //oops: 'Ops!',
+    //tip: 'A página que você está tentando acessar não existe. Não tem certeza do endereço correto?',
+    //home: 'Ir para início'
+//  },//
+  //page_detail: { prev: 'Anterior', next: 'Próximo' }
+//}
+
 export default {
   mate: {
-    description: "Designers, fotógrafos e artistas criativos — mostre aqui o seu melhor trabalho.",
+    description: "Designers, photographers, and creative artists — showcase your best work here.",
     title: "Designer",
   },
-  btns: { backToTop: 'Voltar ao topo' },
-  page_works: { title: '{{name}} - Trabalhos' },
-  footer: { cr: 'Todos os direitos reservados.', icp: '(Río Cuarto, Córdoba, Argentina) ' },
-  menus: { all: 'Tudo', about: 'Sobre', works: 'Trabalhos', contact: 'Contato' },
-  page_404: {
-    oops: 'Ops!',
-    tip: 'A página que você está tentando acessar não existe. Não tem certeza do endereço correto?',
-    home: 'Ir para início'
+  btns: {
+    backToTop: 'Back to Top'
   },
-  page_detail: { prev: 'Anterior', next: 'Próximo' }
+  page_works: {
+    title: '{{name}} - Works',
+  },
+  footer: {
+    cr: 'All rights reserved.',
+    icp: '(Río Cuarto, Córdoba, Argentina) ',
+  },
+  menus: {
+    all: 'All',
+    about: 'About',
+    works: 'Works',
+    contact: 'Contact',
+  },
+  page_404: {
+    oops: 'Oops!',
+    tip: 'The page you are trying to visit does not exist. Not sure about the correct address?',
+    home: 'Go to Home',
+  },
+  page_detail: {
+    prev: 'Previous',
+    next: 'Next',
+  }
 }
+
